c9b42b271944e7db1a482e95727917b5f7db011f
Rajashekar Chinthalapelly <rc@beon.net>
2018-10-22 11:48:52 +0200

    dfgdfg

--- roles/confluent.ssl_CA/scripts/certs-create.sh
+++ roles/confluent.ssl_CA/scripts/certs-create.sh
@@ -6,10 +6,10 @@ set -o nounset \
 #    -o xtrace
 
 # Cleanup files
-rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12 
+rm -f *.crt *.csr *_creds *.jks *.srl *.key *.pem *.der *.p12
 
 # Generate CA key
-openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=hadoop-fra-4.intern.beon.net/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
+openssl req -new -x509 -keyout snakeoil-ca-1.key -out snakeoil-ca-1.crt -days 365 -subj '/CN=ca1.test.confluent.io/OU=TEST/O=CONFLUENT/L=PaloAlto/S=Ca/C=US' -passin pass:confluent -passout pass:confluent
 
 for i in broker client
 do
